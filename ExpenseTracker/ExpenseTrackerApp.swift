0099e4d474bede30719ce2feff4c134fcaea7a58
Zia Ahmed <ziaahmedf17@gmail.com>
2025-09-17 02:49:48 +0500

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Zi on 17/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExpenseTrackerTests/ExpenseTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerTests.swift
+//  ExpenseTrackerTests
+//
+//  Created by Zi on 17/09/2025.
+//
+
+import Testing
+@testable import ExpenseTracker
+
+struct ExpenseTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
